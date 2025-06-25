2f3be582ea91d7371309594696c41c2a35eda9a1
Håkon Hallingstad <hakon@vespa.ai>
2025-06-25 13:15:17 +0200

    Adds zone module defining resource group and networking

---
+++ outputs.tf
@@ -0,0 +1,26 @@
+locals {
+  template_version = "0.1.1"
+  zones_by_env = {
+    for zone in var.all_zones :
+    zone.environment => merge(
+      {
+        // The name of the Vespa Cloud zone, e.g. "prod.azure-eastus-az1"
+        name             = "${zone.environment}.azure-${zone.physical_zone}",
+        // The short name of the Vespa Cloud zone, used e.g. in hostnames
+        short_name       = "${zone.environment}.${zone.physical_zone}",
+        region           = "azure-${zone.physical_zone}",
+        azure_region     = split("-", zone.physical_zone)[0],
+        template_version = local.template_version,
+      },
+      zone
+    )...
+  }
+}
+
+output "zones" {
+    description = "Available zones are listed at https://cloud.vespa.ai/en/reference/zones.html . You reference a zone with `[environment].[region with - replaced by _]` (e.g `prod.azure_eastus_az1`)."
+    value = {
+        for environment, zones in local.zones_by_env :
+        environment => { for zone in zones : replace(zone.region, "-", "_") => zone }
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vespa_cloud_subscription" {
+  description = "Azure subscription ID dedicated to Vespa Cloud Enclave"
+  type        = string
+}
+
+variable "all_zones" {
+  description = "All Azure Vespa Cloud zones"
+  type = list(object({
+    environment   = string
+    physical_zone = string
+  }))
+  default = [
+    { environment = "dev", physical_zone = "eastus-az1" },
+  ]
+}
