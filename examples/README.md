d154898f49cd458e35a2fb6bd90735a596bb25cf
Håkon Hallingstad <hakon@vespa.ai>
2025-07-25 09:52:34 +0200

    Add basic example root module

---
+++ examples/README.md
@@ -0,0 +1,7 @@
+# Vespa Cloud Enclave examples
+
+Here we present to different examples of use of Vespa Cloud Enclave's
+Terraform modules.
+
+[`basic`](./basic/)
+: The most simple minimal example that grants one `dev` zone in the GCP project.
---
+++ examples/basic/main.tf
@@ -0,0 +1,35 @@
+
+#
+# Set up the Azure Terraform Provider to point to the Azure subscription where
+# your want to provision the Vespa Cloud Enclave.
+#
+variable "subscription_id" {
+  type = string
+  default = "<YOUR-SUBSCRIPTION-ID-HERE>"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+provider "azapi" { }
+
+#
+# Set up the basic module that grants Vespa Cloud permission to
+# provision Vespa Cloud resources inside the Azure subscription.
+#
+module "enclave" {
+  source = "github.com/vespa-cloud/terraform-azure-enclave"
+  version     = ">= 1.0.0, < 2.0.0"
+  subscription = var.subscription_id
+}
+
+#
+# Set up the VPC that will contain the Enclave Vespa application for the dev environment.
+#
+module "zone_dev_azure_eastus_az1" {
+  source = "github.com/vespa-cloud/terraform-azure//modules/zone"
+  version = ">= 1.0.0, < 2.0.0"
+  zone = module.enclave.zones.dev.azure_eastus_az1
+}
