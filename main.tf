525cb142433b14adfe84f1386fe4af57453e9c30
gjoranv <gjoranv@gmail.com>
2025-10-20 19:50:03 +0200

    Add clarifying comment about template_version

--- main.tf
+++ main.tf
@@ -10,10 +10,14 @@ terraform {
 }
 
 locals {
+  # NOTE: Do not rename or move this variable!
+  # This is used by github actions to tag releases. Bump whenever making non-trivial changes.
+  # To skip tagging for minor changes, mark the PR with a 'no-tag' label or start the PR title with 'minor'.
   template_version = "0.0.1"
-  zts_prod         = "https://zts.athenz.vespa-cloud.com:4443/zts/v1"
-  zts_cd           = "https://zts.athenz.cd.vespa-cloud.com:4443/zts/v1"
-  issuer_url       = var.__athenz_env == "cd" ? local.zts_cd : local.zts_prod
+
+  zts_prod   = "https://zts.athenz.vespa-cloud.com:4443/zts/v1"
+  zts_cd     = "https://zts.athenz.cd.vespa-cloud.com:4443/zts/v1"
+  issuer_url = var.__athenz_env == "cd" ? local.zts_cd : local.zts_prod
 }
 
 module "provision" {
