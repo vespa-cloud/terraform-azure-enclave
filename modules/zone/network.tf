2810da8a279d6387eb7a51c469566fb645cfa141
gjoranv <gjoranv@gmail.com>
2025-11-04 20:18:17 +0100

    Set shared_access_key_enabled=true for flow logs storage account
    - NSG Flow Logs write to the storage account using account keys
      (not AAD).

--- modules/zone/network.tf
+++ modules/zone/network.tf
@@ -137,7 +137,7 @@ resource "azurerm_storage_account" "flow_logs_storage" {
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
-  shared_access_key_enabled       = false
+  shared_access_key_enabled       = true
 
   tags = {
     zone = var.zone.name
